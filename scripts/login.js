4e177dcbd1b71e1bf7752ab2750f3898a2698644
renepromesse <nrenejordan1@gmail.com>
2020-09-10 15:33:06 +0200

    adds firebase intergration for the admin to login

---
+++ scripts/login.js
@@ -0,0 +1,37 @@
+const db=firebase.firestore();
+const auth= firebase.auth();
+//email: winny@gmail.com
+//password: winny1234
+const form_login= document.getElementById("login-form");
+form_login.addEventListener('submit', event =>{
+    event.preventDefault();
+    const email= form_login['email'].value;
+    const password= form_login['password'].value;
+    if(email != "" && password !=""){
+        auth.signInWithEmailAndPassword(email,password).then( cred =>{
+            console.log('this user logged in: '+cred.user);
+            location.replace('./create-article.html');
+        })
+    }else{
+        console.log('Please enter correct email and password');
+        //document.getElementById('errors').innerHTML="Please check your network or creds";
+    }
+
+
+
+
+    form_login.reset();
+});
+
+
+//lestening to the user status 
+auth.onAuthStateChanged( user =>{
+    if(user){
+        location.replace('./create-article.html');
+    }
+    else{
+        //document.getElementById('errors').innerHTML="Admin Login";
+        console.log('The user is not logged in');
+    }
+});
+
